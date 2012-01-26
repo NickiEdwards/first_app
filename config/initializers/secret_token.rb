a7a2d8a0f3cfa6f3f227d7e25535db61ee02c9cd
Nicki Edwards <edwardsnl08@uww.edu>
2012-01-25 20:18:48 -0600

    Reinstalled Ruby & Rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '00debc9b6ac68fed672ba04ccbd432842c36a9dc8cf823a5bde5e69660461b21ec643be50cb5df54da6b932b34874ae257a06954b9bbb46103ffb8eeda252dc1'
+FirstApp::Application.config.secret_token = 'f2b4c6dad3340ce7819fe33f004f6613b32a1c72d06e46a52cbf05eb5bbd153fab043d9c6bf5dac8012581a54d8badd5376229e21b8f390eb3963ace0eb503bd'
